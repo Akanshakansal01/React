3c011e0548d0f3cafe99193e141917105eed663b
Akanshakansal01 <akanshakansal025@gmail.com>
2023-04-12 14:45:50 +0530

    neww

--- src/components/Person.js
+++ src/components/Person.js
@@ -3,16 +3,29 @@ import React from 'react'
 import './Person.css'
 
 const Person = () => {
+
+    let luckyNumber=7;
+    let randomNumber= Math.floor(Math.random()*10)
+
   return (
     <div style={{color:"brown",padding:"20px"}} className='person'>
+        {
+            (randomNumber === luckyNumber) ? <div>
+            <p> Name : Akansha Kansal </p>
+            <p> Age : 20 </p>
+            <p> Location : Muzaffarnagar, U.P. </p>
+            <img style={{width: "180px"}} 
+            src='https://www.shutterstock.com/image-vector/winners-team-happy-people-win-260nw-1562183173.jpg' alt=''></img>
+            <p> My Number is : {randomNumber} </p>
+            </div>  : <p> You didn't win 😔</p>
+        }
         
+        {/* <p> Name : Akansha Kansal </p>
+        <p> Age : 20 </p>
+        <p> Location : Muzaffarnagar, U.P. </p>
         
-        <p> Name : John Doe </p>
-        <p> Age : 23 </p>
-        <p> Location : New Delhi </p>
-        
-        
-        
+        <p> My Number is : {randomNumber} </p>
+         */}
     </div>
 
   )
