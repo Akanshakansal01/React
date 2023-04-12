93c30971dbe7315b973fd7fa6b68b37ae0ee87f2
Akanshakansal01 <akanshakansal025@gmail.com>
2023-04-12 13:44:30 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+      <p> This is a react component</p>
+      
+    </div>
+  );
+}
+
+export default App;
