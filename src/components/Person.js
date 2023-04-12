c617a0f9a5923d75f599e356a605a97db10652cb
Akanshakansal01 <akanshakansal025@gmail.com>
2023-04-12 14:10:20 +0530

    neww

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,9 +1,20 @@
 // first letter of component must be in Capital letter.
 import React from 'react'
+import './Person.css'
 
 const Person = () => {
   return (
-    <div>Person</div>
+    <div className='person'>
+        
+        
+        <p> Name : John Doe </p>
+        <p> Age : 23 </p>
+        <p> Location : New Delhi </p>
+        
+        
+        
+    </div>
+
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Person from './components/Person';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   // <App />  // self closing tab
   <div>
-    <App />
-    <App />
+    <Person />
+    {/* <App />
+    <App /> */}
   </div>
 
 );
